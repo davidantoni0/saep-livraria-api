f8a15ce17257d1ab19016a284f3727ea2c8835ae
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2026-09-16 21:28:17 -0300

    test: primeiros testes

--- tests/routes/autoresRoutes.test.ts
+++ tests/routes/autoresRoutes.test.ts
@@ -3,14 +3,39 @@ import request from 'supertest';
 import { resetarBanco, fecharBanco } from '../helpers/db';
 import app from '../../src/app';
 
+
 beforeEach(resetarBanco);
 afterAll(fecharBanco);
 
 describe('Rotas de Autor', () => {
-    test.todo('`GET /autores` → **200** e **3** autores');
-    test.todo('`GET /autores/1` → **200**, `nome` = "JRR Tolkien"');
-    test.todo('`GET /autores/999` → **404**');
-    test.todo('`POST /autores` válido (`{ nome, nacionalidade }`) → **201** com `id` no corpo');
+    test('`GET /autores → **200** e **3** autores', async () => {
+        const res = await request(app).get('/autores');
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveLength(3);
+      });
+      test('`GET /autores/1` → **200**, `nome` = "JRR Tolkien"', async () => {
+        const res = await request(app).get('/autores/1');
+        expect(res.status).toBe(200);
+        expect(res.body.nome).toBe('JRR Tolkien');
+    });
+    test('`GET /autores/999` → **404**', async () => {
+        const res = await request(app).get('/autores/999');
+        expect(res.status).toBe(404);
+    });
+    test('`POST /autores` válido (`{ nome, nacionalidade }`) → **201** com `id` no corpo', async () => {
+        const res = await request(app).post('/autores').send(
+          {
+              nome: 'Jk Rowling',
+              nacionalidade: 'Britânica',
+          }
+        );
+        expect(res.status).toBe(201);
+        expect(res.body).toEqual(
+            expect.objectContaining({
+              id: 4
+            })
+          );
+    });
     test.todo('`POST /autores` com body vazio → **400**');
     test.todo('`PUT /autores/1` (`{ nacionalidade }`) → **200** com a nacionalidade nova');
     test.todo('`PUT /autores/999` → **404**');
