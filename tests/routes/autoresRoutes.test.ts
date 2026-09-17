218e8e5315dd12096a3de1fbe1c780ba0ecfa2a1
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2026-09-17 18:47:30 -0300

    test: all exprected tests for autoresRoutes

--- tests/routes/autoresRoutes.test.ts
+++ tests/routes/autoresRoutes.test.ts
@@ -43,10 +43,43 @@ describe('Rotas de Autor', () => {
         );
         expect(res.status).toBe(400);
       });
-    test.todo('`PUT /autores/1` (`{ nacionalidade }`) → **200** com a nacionalidade nova');
-    test.todo('`PUT /autores/999` → **404**');
-    test.todo('`DELETE /autores/3` → **204**');
-    test.todo('`DELETE /autores/999` → **404**');
-    test.todo('`GET /autores/1/livros` → **2** livros');
-    test.todo('`GET /autores/3/livros` → **1** livro');
+    test('`PUT /autores/1` (`{ nacionalidade }`) → **200** com a nacionalidade nova', async () => {
+        const res = await request(app).put('/autores/1')
+        .send(
+            {
+                nacionalidade : 'brasileira'
+            }
+        );
+        
+        expect(res.status).toBe(200);
+        expect(res.body.nacionalidade).toBe('brasileira')
+    });
+    test('`PUT /autores/999` → **404**', async () => {
+        const res = await request(app).put('/autores/999')
+        .send(
+            {
+                nacionalidade : 'brasileira'
+            }
+        );
+        
+        expect(res.status).toBe(404);
+      });
+    test('`DELETE /autores/3` → **204**', async () => {
+        const res = await request(app).delete('/autores/3');
+        expect(res.status).toBe(204);
+    });
+    test('`DELETE /autores/999` → **404**', async () => {
+        const res = await request(app).delete('/autores/999');
+        expect(res.status).toBe(404);
+    });
+    test('`GET /autores/1/livros` → **2** livros', async () => {
+        const res = await request(app).get('/autores/1/livros');
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveLength(2)
+    });
+    test('`GET /autores/3/livros` → **1** livro', async () => {
+        const res = await request(app).get('/autores/3/livros');
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveLength(1)
+    });
 });
